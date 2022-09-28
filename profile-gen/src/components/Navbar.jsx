855c273996e6e07e90d75cf391b7e1c9c401da45
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-09-29 00:56:19 +0530

    navbar

--- profile-gen/src/components/Navbar.jsx
+++ profile-gen/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar-top">
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav"></div>
+        <div className="logos">
+          <img
+            src="https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-vector-shopping-bag-icon-png-image_695741.jpg"
+            alt=""
+            width="40px"
+            height="40px"
+          />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
