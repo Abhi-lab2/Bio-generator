693a9b878cd152fa26dc66a811c3e704ef160f83
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-09-29 15:11:38 +0530

    navbar

--- profile-gen/src/components/Navbar.jsx
+++ profile-gen/src/components/Navbar.jsx
@@ -1,32 +1,16 @@
 import React from "react";
-
 import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar-top">
-      <nav className="navbar navbar-expand-lg navbar-light ">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav"></div>
-        <div className="logos">
-          <img
-            src="https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-vector-shopping-bag-icon-png-image_695741.jpg"
-            alt=""
-            width="40px"
-            height="40px"
-          />
+    <div>
+      <div className="navbar">
+        <div className="navContainer">
+          <div className="navItems">
+            <h1>Bio Generator</h1>
+          </div>
         </div>
-      </nav>
+      </div>
     </div>
   );
 };
